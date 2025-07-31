78ba9742beff6dd00a7fc7546a074df146f6d068
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-31 03:47:18 -0500

    countdown and ascii code added

---
+++ countdown.js
@@ -0,0 +1,15 @@
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+
+/*programming hero*/
+
+let count=81,destination=65;
+
+for(count;count>=destination;count--){
+    console.log(count);
+     console.log(String.fromCharCode(count));
+}
